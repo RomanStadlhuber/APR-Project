68bf2c9de3602d7d9d91f28ab00e912e007d5f97
stefan <stefan.machhammer@gmail.com>
2023-01-07 09:55:53 +0100

    Listerners, Commander with SharedMemory

---
+++ packages/listeners_commander/TCPEchoClient_Commander.cpp
@@ -0,0 +1,212 @@
+#include <stdio.h>      /* for printf() and fprintf() */
+#include <sys/socket.h> /* for socket(), connect(), send(), and recv() */
+#include <arpa/inet.h>  /* for sockaddr_in and inet_addr() */
+#include <stdlib.h>     /* for atoi() and exit() */
+#include <string.h>     /* for memset() */
+#include <unistd.h>     /* for close() */
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <sys/sem.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#include "shared_memory.hpp"
+
+#define BLOCK_SIZE 4096
+
+int sock;                        /* Socket descriptor */
+sem_t *sem_prod;
+sem_t *sem_cons;
+sem_t *sem_prod2;
+sem_t *sem_cons2;
+struct SharedMemoryLIDAR *block;
+struct SharedMemoryODO *block2;
+
+//Compile mit:     g++ TCPEchoClient_Commander.cpp -lpthread -o TCPEchoClient_Commander
+//Start mit:       ./TCPEchoClient_Commander 127.0.0.1 10001
+
+void signalHandler (int sig)
+{
+    printf("Close all\n");
+    sem_close(sem_cons2);
+    sem_close(sem_prod2);
+    sem_close(sem_cons);
+    sem_close(sem_prod);
+
+    detach_memory_block_LIDAR(block);
+    detach_memory_block_Odometrie(block2);
+
+
+    close(sock);
+    exit(0);
+
+}  // end producerHandler
+
+void greateSemahpore()
+{
+    sem_unlink(SEM_CONSUMER_FNAME);
+    sem_unlink(SEM_PRODUCER_FNAME);
+    sem_unlink(SEM_CONSUMER2_FNAME);
+    sem_unlink(SEM_PRODUCER2_FNAME);
+
+    sem_prod = sem_open(SEM_PRODUCER_FNAME, O_CREAT, 0660, 0);
+    if(sem_prod == SEM_FAILED)
+    {
+        //perror("sem_open/producer");
+        printf("open Semaphore failed\n");
+        exit(EXIT_FAILURE);    
+    }
+
+    sem_cons = sem_open(SEM_CONSUMER_FNAME, O_CREAT, 0660, 1);
+    if(sem_cons == SEM_FAILED)
+    {
+        //perror("sem_open/consumer");
+        printf("open Semaphore failed\n");
+        exit(EXIT_FAILURE);    
+    }
+    //printf("open Semaphore succesfull\n");
+      sem_prod2 = sem_open(SEM_PRODUCER2_FNAME, O_CREAT, 0660, 0);
+    if(sem_prod2 == SEM_FAILED)
+    {
+        //perror("sem_open/producer");
+        printf("open Semaphore failed\n");
+        exit(EXIT_FAILURE);    
+    }
+
+    sem_cons2 = sem_open(SEM_CONSUMER2_FNAME, O_CREAT, 0660, 1);
+    if(sem_cons2 == SEM_FAILED)
+    {
+        //perror("sem_open/consumer");
+        printf("open Semaphore failed\n");
+        exit(EXIT_FAILURE);    
+    }
+}
+
+struct SharedMemoryLIDAR* readSharedMemoryLidar()
+{
+   printf("Herer...\n");
+        block = attach_memory_block_LIDAR(FILENAME);
+        if(block == NULL)
+            {
+                printf("Error: could not get block\n");
+                //return -1;
+            }
+    //printf("Reading: \"%s\"\n", block);
+    return block;
+}
+
+struct SharedMemoryODO* readSharedMemoryOdometrie()
+{
+   printf("Herer...\n");
+        block2 = attach_memory_block_Odometrie(FILENAME2);
+        if(block2 == NULL)
+            {
+                printf("Error: could not get block\n");
+                //return -1;
+            }
+    //printf("Reading: \"%s\"\n", block);
+    return block2;
+}
+
+
+int main(int argc, char *argv[])
+{
+
+    struct sockaddr_in echoServAddr; /* Echo server address */
+    unsigned short echoServPort;     /* Echo server port */
+    char *servIP;                    /* Server IP address (dotted quad) */
+    unsigned int bytesRcvd, totalBytesRcvd;   /* Bytes read in single recv()  // changed to unsigned int due to comparison problems
+                                        and total bytes read */
+
+    if ((argc < 3) || (argc > 3))    /* Test for correct number of arguments */
+    {
+       fprintf(stderr, "Usage: %s <Server IP> [<Echo Port>]\n",
+               argv[0]);
+       exit(1);
+    }
+
+    servIP = argv[1];             /* First arg: server IP address (dotted quad) */
+
+    if (argc == 3)
+        echoServPort = atoi(argv[2]); /* Use given port, if any */
+    else
+        echoServPort = 7;  /* 7 is the well-known port for the echo service */
+
+    /* Create a reliable, stream socket using TCP */
+    if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+        printf("socket() failed");
+
+    /* Construct the server address structure */
+    memset(&echoServAddr, 0, sizeof(echoServAddr));     /* Zero out structure */
+    echoServAddr.sin_family      = AF_INET;             /* Internet address family */
+    echoServAddr.sin_addr.s_addr = inet_addr(servIP);   /* Server IP address */
+    echoServAddr.sin_port        = htons(echoServPort); /* Server port */
+
+    /* Establish the connection to the echo server */
+    if (connect(sock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
+        printf("connect() failed");
+
+  //------------------------------------------------------------------------
+     
+    greateSemahpore();
+    signal (SIGINT, signalHandler);  // catch SIGINT
+    struct SharedMemoryLIDAR* test;
+    struct SharedMemoryODO* test2;
+
+    while(1)
+       { 
+        printf("Waiting...\n");
+        
+        sem_wait(sem_prod);
+        test = readSharedMemoryLidar();
+      
+        printf("Reading Lidar: \"%d\"\n", test->testData);
+
+        sem_post(sem_cons2);
+
+        sem_wait(sem_prod2);
+        test2 = readSharedMemoryOdometrie();
+      
+        printf("Reading Odometrie: \"%d\"\n", test2->testData);
+
+        //------------------------------------------------------------------------
+        //Great Message-String
+        float lin = 0.3;
+        float angular = 1;
+        std::ostringstream oss;
+        //"---START---{\"linear\": 0.1, \"angular\": 0.10}___END___\0";
+        oss << "---START---{\"linear\": "<< lin <<", \"angular\": "<< angular <<"}___END___\0";
+        //std::cout << oss.str();
+        std::string str = oss.str();
+        const char * echoString = str.c_str();
+        unsigned int echoStringLen = strlen(echoString);      /* Length of string to echo */
+        
+        //------------------------------------------------------------------------
+
+        /* Send the string to the server */
+        if (send(sock, echoString, echoStringLen, 0) != echoStringLen)
+            printf("send() sent a different number of bytes than expected");
+
+        printf("%s\n", echoString);
+        sleep(1);
+        sem_post(sem_cons);
+       }
+
+    sem_close(sem_cons);
+    sem_close(sem_prod);
+    sem_close(sem_cons2);
+    sem_close(sem_prod2);
+
+    detach_memory_block_LIDAR(block);
+    detach_memory_block_Odometrie(block2);
+    close(sock);
+    exit(0);
+   
+}
---
+++ packages/listeners_commander/TCPEchoClient_Lidar.cpp
@@ -0,0 +1,181 @@
+#include <stdio.h>      /* for printf() and fprintf() */
+#include <sys/socket.h> /* for socket(), connect(), send(), and recv() */
+#include <arpa/inet.h>  /* for sockaddr_in and inet_addr() */
+#include <stdlib.h>     /* for atoi() and exit() */
+#include <string.h>     /* for memset() */
+#include <unistd.h>     /* for close() */
+#include <sys/sem.h>
+#include <iostream>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <signal.h>
+
+#include "shared_memory.hpp"
+
+#define BLOCK_SIZE 4096
+
+int sock;                        /* Socket descriptor */
+sem_t *sem_prod;
+sem_t *sem_cons;
+struct SharedMemoryLIDAR *block;
+
+#define RCVBUFSIZE 14000   /* Size of receive buffer */
+
+//Compile with:  g++ TCPEchoClient_Lidar.cpp shared_memory.cpp -lpthread  -o TCPEchoClient_Lidar
+//Start with: ./TCPEchoClient_Lidar
+
+void signalHandler (int sig)
+{
+    printf("Close all\n");
+    sem_close(sem_cons);
+    sem_close(sem_prod);
+
+    detach_memory_block_LIDAR(block);
+    if(destroy_memory_block(FILENAME))
+    {
+        printf("Destroyed block: %s\n", FILENAME);
+    }
+    else{
+        printf("Could not destroy block: %s\n", FILENAME);
+    }
+    close(sock);
+    exit(0);
+
+}  // end producerHandler
+
+int greatSharedMemory()
+{
+    //Setup some semaphores
+    sem_prod = sem_open(SEM_PRODUCER_FNAME, 0);
+    if(sem_prod == SEM_FAILED)
+    {
+        printf("open Semaphore failed");
+        //perror("sem_open/producer");
+        exit(EXIT_FAILURE);    
+    }
+
+    sem_cons = sem_open(SEM_CONSUMER_FNAME, 0);
+    if(sem_cons == SEM_FAILED)
+    {
+        printf("open Semaphore failed");
+        //perror("sem_open/consumer");
+        exit(EXIT_FAILURE);    
+    }
+
+    //grab the shared memory block
+   
+    block = attach_memory_block_LIDAR(FILENAME);
+    if(block == NULL)
+    {
+        printf("Error: could not get block\n");
+        return -1;
+    }
+    
+    return 0;
+}
+
+
+void writeSharedMemory(struct SharedMemoryLIDAR *block, struct SharedMemoryLIDAR *data)
+{
+    block->testData = data->testData;
+    printf("Writing: \"%d\"\n", block->testData);
+    //strncpy(block, testChar, BLOCK_SIZE);
+}
+
+std::string getMessage(const std::string &buffer, const std::string &start_delimimter, const std::string &ende_delimimter)
+{
+    //std::cout << s << std::endl;
+    unsigned pos_start_delimimter = buffer.find(start_delimimter, 0);
+    unsigned pos_ende_delimimter = buffer.find(ende_delimimter, pos_start_delimimter);
+    printf("\n first delim pos: %d | last delim pos: %d \n", pos_start_delimimter, pos_ende_delimimter );
+    
+    return buffer.substr(pos_start_delimimter, pos_ende_delimimter + ende_delimimter.length() - pos_start_delimimter);
+}
+
+
+int main(int argc, char *argv[])
+{
+    //int sock;                        /* Socket descriptor */
+    struct sockaddr_in echoServAddr; /* Echo server address */
+    unsigned short echoServPort;     /* Echo server port */
+    char *servIP;                    /* Server IP address (dotted quad) */
+    char const *echoString;                /* String to send to echo server */
+    char echoBuffer[RCVBUFSIZE];     /* Buffer for echo string */
+    unsigned int echoStringLen;      /* Length of string to echo */
+    int bytesRcvd, totalBytesRcvd;   /* Bytes read in single recv() 
+                                        and total bytes read */
+
+    if ((argc < 2) || (argc > 3))    /* Test for correct number of arguments */
+    {
+       fprintf(stderr, "Usage: %s <Server IP> [<Echo Port>]\n",
+               argv[0]);
+       exit(1);
+    }
+
+    servIP = argv[1];             /* First arg: server IP address (dotted quad) */
+    echoString = argv[2];         /* Second arg: string to echo */
+
+    if (argc == 3)
+        echoServPort = atoi(argv[2]); /* Use given port, if any */
+    else
+        echoServPort = 7;  /* 7 is the well-known port for the echo service */
+
+    /* Create a reliable, stream socket using TCP */
+    if ((sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0)
+        printf("socket() failed");
+
+    /* Construct the server address structure */
+    memset(&echoServAddr, 0, sizeof(echoServAddr));     /* Zero out structure */
+    echoServAddr.sin_family      = AF_INET;             /* Internet address family */
+    echoServAddr.sin_addr.s_addr = inet_addr(servIP);   /* Server IP address */
+    echoServAddr.sin_port        = htons(echoServPort); /* Server port */
+
+    /* Establish the connection to the echo server */
+    if (connect(sock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0)
+        printf("connect() failed");
+
+    //---------------------------------------------------------------------------   
+    sleep(1);
+    greatSharedMemory();
+    signal (SIGINT, signalHandler);  // catch SIGINT
+    
+    struct SharedMemoryLIDAR *test = new SharedMemoryLIDAR();
+    test->testData = 5;
+
+    //---------------------------------------------------------------------------   
+
+    echoStringLen = strlen(echoString);          /* Determine input length */
+
+    /* Receive the same string back from the server */
+    totalBytesRcvd = 0;
+    printf("Received: ");                /* Setup to print the echoed string */
+    while (1)
+    {
+        /* Receive up to the buffer size (minus 1 to leave space for
+           a null terminator) bytes from the sender */
+          
+        sem_wait(sem_cons);
+        if ((bytesRcvd = recv(sock, echoBuffer, RCVBUFSIZE - 1, 0)) <= 0)
+            printf("recv() failed or connection closed prematurely");
+        totalBytesRcvd += bytesRcvd;   /* Keep tally of total bytes */
+        echoBuffer[bytesRcvd] = '\0';  /* Terminate the string! */
+        //printf("%s", echoBuffer);      /* Print the echo buffer */
+        std::cout << getMessage(echoBuffer, "--START---", "___END___") << std::endl; 
+        
+        writeSharedMemory(block, test);
+        test->testData++;   
+        sem_post(sem_prod);
+        printf("waiting\n");
+      
+       
+    }
+    printf("\n");    /* Print a final linefeed */
+    
+    sem_close(sem_cons);
+    sem_close(sem_prod);
+
+    detach_memory_block_LIDAR(block);
+    close(sock);
+    exit(0);
+   
+}
